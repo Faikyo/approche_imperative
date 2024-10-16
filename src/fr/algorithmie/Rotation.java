5365a153801aff00d731d9d194cafeb4ace73803
Furkan Demirbas <m.furkan0508@gmail.com>
2024-10-16 16:22:32 +0200

    Rotation

---
+++ src/fr/algorithmie/Rotation.java
@@ -0,0 +1,22 @@
+package fr.algorithmie;
+
+public class Rotation {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+
+        int lastElement = array[array.length - 1];
+
+        for (int i = array.length - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[0] = lastElement;
+
+        for (int n : array) {
+            System.out.print(n + " ");
+        }
+    }
+
+}
