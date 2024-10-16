a3a117b4a2bca0673d5441df9baf454ef33476d2
Furkan Demirbas <m.furkan0508@gmail.com>
2024-10-16 10:07:23 +0200

    AffichageIdentite

---
+++ src/fr/algorithmie/AffichageIdentite.java
@@ -0,0 +1,10 @@
+package fr.algorithmie;
+
+public class AffichageIdentite {
+
+    public static void main(String[] args) {
+        for (int i=1; i<=10;i++){
+            System.out.println("Furkan Demirbas "+i);
+        }
+    }
+}
