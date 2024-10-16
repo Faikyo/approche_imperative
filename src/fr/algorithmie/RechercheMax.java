677bf08c1dd8e6beeef0b8db48b57cc00d762478
Furkan Demirbas <m.furkan0508@gmail.com>
2024-10-16 14:36:14 +0200

    RechercheMax

---
+++ src/fr/algorithmie/RechercheMax.java
@@ -0,0 +1,18 @@
+package fr.algorithmie;
+
+public class RechercheMax {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+
+        int max = array[0];
+
+        for(int i=1; i< array.length;i++){
+            if(max<array[i]){
+                max=array[i];
+            }
+        }
+        System.out.println("La valeur la plus grande du tableau est " +max);
+    }
+}
