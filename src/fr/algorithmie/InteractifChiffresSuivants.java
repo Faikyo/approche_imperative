6a53cd107f1ce43a729a9b1ad7f684249f334e62
Furkan Demirbas <m.furkan0508@gmail.com>
2024-10-17 09:27:34 +0200

    InteractifChiffresSuivant

---
+++ src/fr/algorithmie/InteractifChiffresSuivants.java
@@ -0,0 +1,21 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+public class InteractifChiffresSuivants {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez un nombre : ");
+        int nb = scanner.nextInt();
+
+        System.out.println("Les 10 nombres suivants sont : ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print((nb + i) + " ");
+        }
+
+        scanner.close();
+    }
+}
